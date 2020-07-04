1960bd42e528cb39836ac253868b44b3b467f36c
Mehul Dantani <mehuldantani97@gmail.com>
2020-07-04 23:03:36 +0530

    API
    changes made for signup and login API

--- api/modules/user.js
+++ api/modules/user.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userschema = mongoose.Schema({
  _id :mongoose.Types.ObjectId,
- email:{ type: String, required: true},
+ email:{ type: String,
+        required: true,
+        unique: true,
+        match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/},
  password: {type: String, required: true},
  name: {type: String, required : true}
 })
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,45 +1,100 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const JWT = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const user = require('../modules/user');
 const bcrypt = require('bcrypt');
 
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
-router.post("/signup", jsonParser, (req, res) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err){
-            return res.status(500).json({
-                error: err
+const JWT_token_key = "gupshup_secret";
+
+router.post("/signup", jsonParser, (req, res, next) => {
+    user.find({ email: req.body.email})
+    .exec()
+    .then(User => {
+        if (User.length >= 1){
+            console.log(User);
+            return res.status(409).json({
+                message: "User already exists."
             })
         }
         else{
-            const adduser = new user({
-                _id: mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password : hash,
-                name: req.body.name
-            });
-            adduser
-            .save()
-            .then(res =>{
-                return res.status(201).json({
-                    message: 'User Created'
-                });
-            })
-            .catch(err =>{
-                console.log(err);
-                res.status(500).json({
-                    message: err
-                });
-            })
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if(err){
+                    return res.status(500).json({
+                        error: err
+                    })
+                }
+                else{
+                    const adduser = new user({
+                        _id: mongoose.Types.ObjectId(),
+                        email: req.body.email,
+                        password : hash,
+                        name: req.body.name
+                    });
+                    adduser
+                    .save()
+                    .then(result => {
+                        console.log(result);
+                        res.status(201).json({
+                          message: "User created"
+                        });
+                      })
+                    .catch(err =>{
+                        console.log(err);
+                        res.status(500).json({
+                            message: err
+                        });
+                    })
+        
+                }
+                }
+            
+            );
+        }
+    })    
 
-        }
-        }
-    
-    );
 
 });
+
+ router.post("/login", jsonParser, (req, res, next) => {
+     user.find({email: req.body.email})
+     .exec()
+     .then(User => {
+         if(User.length <1){
+           return res.status(401).json({
+                messsage: "Auth Failed"
+            })
+         }
+         bcrypt.compare(req.body.password, User[0].password, (err, result)=>{
+             if(err){
+                  return res.status(401).json({
+                     message:"Auth Failed"
+                 })
+             }
+             if(result){
+                 const token = JWT.sign({
+                     email: User[0].email,
+                     user_id : User[0]._id
+                 },
+                 JWT_token_key,
+                 {
+                     expiresIn: "1hr"
+                 })
+                 return res.status(200).json({
+                     message:"Auth Successful",
+                     token: token
+                 })
+             }
+             return res.status(401).json({
+                 message:"Auth Failed"
+             })
+         })
+     })
+     .catch()
+ })
+ 
 
 module.exports = router;
