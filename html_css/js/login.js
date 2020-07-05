23f649f2e82eeb46d02c8b252eb0526f4b29d34b
Mehul Dantani <mehuldantani97@gmail.com>
2020-07-05 21:32:35 +0530

    login and Signup are now possible with using API

--- html_css/js/login.js
+++ html_css/js/login.js
@@ -6,9 +6,6 @@ const back = document.getElementById('back');
 const user_sign_up = document.getElementById('user_sign_up');
 const user_sign_in = document.getElementById('user_sign_in');
 
-const email = document.getElementById('login_id');
-const password = document.getElementById('login_pw');
-
 //adding eventlister for signup button for CSS animation
 signUpbtn.addEventListener('click',()=>{
 	container.classList.add('right-panel-active')
@@ -29,23 +26,65 @@ back.addEventListener('click',()=>{
 	container.classList.remove('forgot-panel-active')
 });
 
+//adding eventlistener when user clicks login
 user_sign_in.addEventListener('click', ()=>{
 
-fetch("http://localhost:5500/user/login", {
+//get email id and password
+const email_id = document.getElementById('login_id').value;
+const pword = document.getElementById('login_pw').value;
+ 
+//call API and check credentials.
+fetch('http://localhost:5500/user/login', {
   method: 'POST',
   headers: {
     'content-type': 'application/json',
   },
-  body: {
-    "email": email,
-    "password": password
-}
-})
-  .then(response => {
-	console.log(response);
-	console.log('api done');
+  body: JSON.stringify({
+    email: email_id,
+    password: pword
   })
+})
+  .then(response => response.json())
+  .then(data => {
+    //if API response is successful then redirect to chat page
+    if(data.message === "Auth Successful")
+    window.location.href ="http://localhost:5500/chat.html"
+  }
+    )
   .catch(err => {
     console.log(err)
   })
 });
+
+
+//adding eventlistener when user clicks signup
+user_sign_up.addEventListener('click', ()=>{
+
+  //get email id and password
+  const email_id = document.getElementById('signup_email_id').value;
+  const pword = document.getElementById('signup_pword').value;
+  const name = document.getElementById('signup_name').value;
+   
+  //call API and check credentials.
+  fetch('http://localhost:5500/user/signup', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email_id,
+      password: pword,
+      name: name
+    })
+  })
+    .then(response => response.json())
+    .then(data => {
+      //if API response is successful then redirect to chat page
+      if(data.message === "User created")
+      window.location.href ="http://localhost:5500/chat.html"
+    }
+      )
+    .catch(err => {
+      console.log(err)
+    })
+  });
