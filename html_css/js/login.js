7f3f412dce2212057aee10054b0e2c3735fdbfa0
Mehul Dantani <mehuldantani97@gmail.com>
2020-07-05 14:38:20 +0530

    .

--- html_css/js/login.js
+++ html_css/js/login.js
@@ -6,6 +6,9 @@ const back = document.getElementById('back');
 const user_sign_up = document.getElementById('user_sign_up');
 const user_sign_in = document.getElementById('user_sign_in');
 
+const email = document.getElementById('login_id');
+const password = document.getElementById('login_pw');
+
 //adding eventlister for signup button for CSS animation
 signUpbtn.addEventListener('click',()=>{
 	container.classList.add('right-panel-active')
@@ -26,3 +29,23 @@ back.addEventListener('click',()=>{
 	container.classList.remove('forgot-panel-active')
 });
 
+user_sign_in.addEventListener('click', ()=>{
+
+fetch("http://localhost:5500/user/login", {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+  },
+  body: {
+    "email": email,
+    "password": password
+}
+})
+  .then(response => {
+	console.log(response);
+	console.log('api done');
+  })
+  .catch(err => {
+    console.log(err)
+  })
+});
